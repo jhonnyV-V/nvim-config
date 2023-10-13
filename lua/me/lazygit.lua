b52755d05408b1cb79ca621319eec268667eaf94
jhonnyV-V <jhonny_varela_visbal@hotmail.com>
2023-10-13 18:23:44 -0400

    making floaterm terminal bigger

--- lua/me/lazygit.lua
+++ lua/me/lazygit.lua
@@ -1,1 +1,1 @@
-vim.keymap.set("n", "<leader>lz", "<cmd> FloatermNew! --disposable lazygit<CR>" )
+vim.keymap.set("n", "<leader>fg", "<cmd> FloatermNew! --disposable --width=0.9 --heigth=0.9 lazygit<CR>" )
