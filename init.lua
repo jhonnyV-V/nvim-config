4fa06e9885c6859445c85696f5864f75872aad4d
Jhonny Varela <jhonny_varela_visbal@hotmail.com>
2024-03-05 19:24:31 -0400

    changing theme

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ require('lazy').setup {
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
-			vim.cmd.colorscheme 'tokyonight-night'
+			vim.cmd.colorscheme 'tokyonight-storm'
 
 			-- You can configure highlights by doing something like
 			vim.cmd.hi 'Comment gui=none'
--- lua/custom/remaps.lua
+++ lua/custom/remaps.lua
@@ -1,68 +1,49 @@
-vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = 'Save File' })
-vim.keymap.set("n", "<leader><leader>", ":so<CR>", { desc = 'Source lua file' })
+vim.keymap.set('n', '<leader>w', ':w<CR>', { desc = 'Save File' })
+vim.keymap.set('n', '<leader><leader>', ':so<CR>', { desc = 'Source lua file' })
 
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move Selected Down' })
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move Selected Up' })
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = 'Move Selected Down' })
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = 'Move Selected Up' })
+vim.keymap.set('n', 'J', 'mzJ`z', { desc = 'Stack next line on current' })
 
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Jump half page down' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Jump half page up' })
 
-vim.keymap.set("n", "J", "mzJ`z", { desc = 'Stack next line on current' })
+vim.keymap.set('n', 'n', 'nzzzv', { desc = 'Next search result' })
+vim.keymap.set('n', 'N', 'Nzzzv', { desc = 'Previous search result' })
 
-
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = 'Jump half page down' })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = 'Jump half page up' })
-
-vim.keymap.set("n", "n", "nzzzv", { desc = 'Next search result' })
-vim.keymap.set("n", "N", "Nzzzv", { desc = 'Previous search result' })
-
-vim.keymap.set("x", "<leader>p", [["_dP]], { desc = 'Paste without yanking' })
+vim.keymap.set('x', '<leader>p', [["_dP]], { desc = 'Paste without yanking' })
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = 'Yank to the + register' })
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]], { desc = 'Yank to the + register' })
 
-vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = 'Yank line to the + register' })
+vim.keymap.set('n', '<leader>Y', [["+Y]], { desc = 'Yank line to the + register' })
 
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = 'Delete to the void' })
-vim.keymap.set("n", "<leader>D", [["_D]], { desc = 'Delete line to the void (_ register)' })
+vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]], { desc = 'Delete to the void' })
+vim.keymap.set('n', '<leader>D', [["_D]], { desc = 'Delete line to the void (_ register)' })
 
+vim.keymap.set('n', '<leader>tm', '<cmd>silent !tmux neww tmux-sessionizer<CR>', { desc = 'Open Tmux-sessionizer' })
 
-vim.keymap.set(
-  "n",
-  "<leader>tm",
-  "<cmd>silent !tmux neww tmux-sessionizer<CR>",
-  { desc = 'Open Tmux-sessionizer' }
-)
-
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = 'Lsp Format' })
+vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { desc = 'Lsp Format' })
 
 -- Quick fix navigation
-vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz", { desc = 'Next QuickFix Item' })
-vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz", { desc = 'Previous QuickFix Item' })
+vim.keymap.set('n', '<C-j>', '<cmd>cnext<CR>zz', { desc = 'Next QuickFix Item' })
+vim.keymap.set('n', '<C-k>', '<cmd>cprev<CR>zz', { desc = 'Previous QuickFix Item' })
 --not sure what it does yet
-vim.keymap.set("n", "<leader>j", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lprev<CR>zz")
+vim.keymap.set('n', '<leader>j', '<cmd>lnext<CR>zz')
+vim.keymap.set('n', '<leader>k', '<cmd>lprev<CR>zz')
 
-vim.keymap.set(
-  "n",
-  "<leader>s",
-  [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
-  { desc = 'Replace word in file' }
-)
+vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = 'Replace word in file' })
 
-vim.keymap.set(
-  "n",
-  "<leader>x",
-  "<cmd>!chmod +x %<CR>",
-  {
-    silent = true,
-    desc = 'Make current file an executable (chmod)'
-  }
-)
+vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', {
+	silent = true,
+	desc = 'Make current file an executable (chmod)',
+})
 
 -- Diagnostic keymaps
 -- TODO: check for duplication
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', '[d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
@@ -86,9 +67,9 @@ vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	desc = 'Highlight when yanking (copying) text',
+	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
