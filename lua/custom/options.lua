34702fa05950983dbd2f18bddc11078a8c8b0310
JhonnyV-V <jhonny_varela_visbal@hotmail.com>
2024-04-13 11:19:53 -0400

    delete custom clipboard

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -87,18 +87,3 @@ vim.opt.incsearch = true
 vim.opt.scrolloff = 8
 
 vim.opt.fileformat = 'unix'
-
-if vim.fn.has 'wsl' then
-	vim.g.clipboard = {
-		name = 'win32yank-wsl',
-		copy = {
-			['+'] = 'win32yank.exe -i --crlf',
-			['*'] = 'win32yank.exe -i --crlf',
-		},
-		paste = {
-			['+'] = 'win32yank.exe -o --lf',
-			['*'] = 'win32yank.exe -o --lf',
-		},
-		cache_enable = 0,
-	}
-end
