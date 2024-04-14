95f994bda68e0481143ea3c85d421b3f952237a9
JhonnyV-V <jhonny_varela_visbal@hotmail.com>
2024-04-14 15:32:33 -0400

    adding cloak

--- init.lua
+++ init.lua
@@ -124,6 +124,7 @@ require('lazy').setup {
 	require 'custom.plugins.autocompletion',
 	require 'custom.plugins.automaton',
 	require 'custom.plugins.blackjack-game',
+	require 'custom.plugins.cloak-config',
 	require 'custom.plugins.codeium-config',
 	require 'custom.plugins.codesnaps-config',
 	require 'custom.plugins.color-picker-config',
