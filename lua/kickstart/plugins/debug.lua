d67fa1e3a15a09ff49796e363fae3a69795e1adf
jhonnyV-V <jhonny_varela_visbal@hotmail.com>
2024-05-18 18:23:10 -0400

    adding go debug keymap

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -47,6 +47,7 @@ return {
 		vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
 		vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
 		vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
+		vim.keymap.set('n', '<leader>td', dap.terminate, { desc = 'Debug: Terminate' })
 		vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
 		vim.keymap.set('n', '<leader>B', function()
 			dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
@@ -90,7 +91,9 @@ return {
 		dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
 		-- Install golang specific config
-		require('dap-go').setup()
+		local dap_go = require('dap-go')
+		dap_go.setup()
+		vim.keymap.set('n', '<leader>dt', dap_go.debug_test, { desc = 'Debug Go: Debug Test' })
 		-- WARNING: set dlv in path or it will not work
 	end,
 }
