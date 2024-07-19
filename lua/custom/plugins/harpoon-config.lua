19247b1ef99031b36f14578e9110de4f0468cfa2
jhonnyV-V <jhonny_varela_visbal@hotmail.com>
2024-07-19 12:25:20 -0400

    new harpoon keys

--- lua/custom/plugins/harpoon-config.lua
+++ lua/custom/plugins/harpoon-config.lua
@@ -18,25 +18,25 @@ return {
     )
     vim.keymap.set(
       "n",
-      "<C-h>",
+      "<C-j>",
       function() ui.nav_file(1) end,
       { desc = 'Harpoon file 1' }
     )
     vim.keymap.set(
       "n",
-      "<C-t>",
+      "<C-k>",
       function() ui.nav_file(2) end,
       { desc = 'Harpoon file 2' }
     )
     vim.keymap.set(
       "n",
-      "<C-n>",
+      "<C-h>",
       function() ui.nav_file(3) end,
       { desc = 'Harpoon file 3' }
     )
     vim.keymap.set(
       "n",
-      "<C-m>",
+      "<C-l>",
       function() ui.nav_file(4) end,
       { desc = 'Harpoon file 4' }
     )
