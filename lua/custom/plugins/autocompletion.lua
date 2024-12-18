b36b69bf78778ec3d616a6454af7415d76005980
jhonnyV-V <jhonny_varela_visbal@hotmail.com>
2024-12-18 15:37:13 -0400

    changing autocompletion engine

--- lua/custom/plugins/autocompletion.lua
+++ lua/custom/plugins/autocompletion.lua
@@ -1,8 +1,7 @@
-return { -- Autocompletion
-	'hrsh7th/nvim-cmp',
-	event = 'InsertEnter',
-	dependencies = {
-		-- Snippet Engine & its associated nvim-cmp source
+return {
+  'saghen/blink.cmp',
+  dependencies = {
+		'rafamadriz/friendly-snippets',
 		{
 			'L3MON4D3/LuaSnip',
 			build = (function()
@@ -14,72 +13,43 @@ return { -- Autocompletion
 				end
 				return 'make install_jsregexp'
 			end)(),
-			dependencies = { 'rafamadriz/friendly-snippets' },
+			dependencies = 'rafamadriz/friendly-snippets',
 		},
-		'saadparwaiz1/cmp_luasnip',
-		'hrsh7th/cmp-nvim-lsp',
-		'hrsh7th/cmp-path',
 	},
-	config = function()
-		-- See `:help cmp`
-		local cmp = require 'cmp'
-		local luasnip = require 'luasnip'
-		luasnip.config.setup {}
-		require('luasnip.loaders.from_vscode').lazy_load()
+  version = 'v0.*',
+  opts = {
+    keymap = { preset = 'default' },
 
-		cmp.setup {
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			completion = { completeopt = 'menu,menuone,noinsert' },
+    appearance = {
+      -- will be removed in a future release
+      use_nvim_cmp_as_default = true,
+      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      nerd_font_variant = 'mono'
+    },
 
-			-- For an understanding of why these mappings were
-			-- chosen, you will need to read `:help ins-completion`
-			--
-			-- No, but seriously. Please read `:help ins-completion`, it is really good!
-			mapping = cmp.mapping.preset.insert {
-				['<C-n>'] = cmp.mapping.select_next_item(),
-				['<C-p>'] = cmp.mapping.select_prev_item(),
-				-- Accept ([y]es) the completion.
-				--  This will auto-import if your LSP supports it.
-				--  This will expand snippets if the LSP sent a snippet.
-				['<C-y>'] = cmp.mapping.confirm { select = true },
-				-- Manually trigger a completion from nvim-cmp.
-				--  Generally you don't need this, because nvim-cmp will display
-				--  completions whenever it has completion options available.
-				['<C-Space>'] = cmp.mapping.complete {},
-				-- Think of <c-l> as moving to the right of your snippet expansion.
-				--  So if you have a snippet that's like:
-				--  function $name($args)
-				--    $body
-				--  end
-				--
-				-- <c-l> will move you to the right of each of the expansion locations.
-				-- <c-h> is similar, except moving you backwards.
-				['<C-l>'] = cmp.mapping(function()
-					if luasnip.expand_or_locally_jumpable() then
-						luasnip.expand_or_jump()
-					end
-				end, { 'i', 's' }),
-				['<C-h>'] = cmp.mapping(function()
-					if luasnip.locally_jumpable(-1) then
-						luasnip.jump(-1)
-					end
-				end, { 'i', 's' }),
-			},
-			sources = {
-				{ name = 'nvim_lsp' },
-				{ name = 'luasnip' },
-				{ name = 'path' },
-			},
-		}
-		cmp.setup.filetype({ 'sql' }, {
-			sources = {
-				{ name = 'vim-dadbod-completion' },
-				{ name = 'buffer' },
-			},
-		})
-	end,
+		snippets = {
+			expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
+      active = function(filter)
+        if filter and filter.direction then
+          return require('luasnip').jumpable(filter.direction)
+        end
+        return require('luasnip').in_snippet()
+      end,
+      jump = function(direction) require('luasnip').jump(direction) end,
+		},
+
+    -- default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, via `opts_extend`
+    sources = {
+      default = { 'lsp', 'path', 'luasnip', 'snippets', 'buffer' },
+      -- optionally disable cmdline completions
+      -- cmdline = {},
+    },
+
+    -- experimental signature help support
+    signature = { enabled = true }
+  },
+  -- allows extending the providers array elsewhere in your config
+  -- without having to redefine it
+  opts_extend = { "sources.default" }
 }
