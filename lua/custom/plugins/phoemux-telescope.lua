f9f285f7609239f55457e95d09de7221dbe64f2d
jhonnyV-V <jhonny_varela_visbal@hotmail.com>
2025-03-15 16:15:31 -0400

    more pheomux-telescope stuff

--- lua/custom/plugins/phoemux-telescope.lua
+++ lua/custom/plugins/phoemux-telescope.lua
@@ -1,5 +1,9 @@
+---@type lazydev.Library.spec
 return {
 	'jhonnyV-V/phoemux-telescope',
+	-- name = "phoemux-telescope",
+	-- dev = true,
+	-- dir = "~/Code/lua/phoemux-telescope",
 	dependencies = {
 		'nvim-telescope/telescope.nvim',
 	},
@@ -8,9 +12,51 @@ return {
 		{
 			'<leader>ms',
 			function()
-				require('phoemux-telescope').phoemux()
+				---@type Phoemux
+				local phoemux = require('phoemux-telescope')
+				phoemux.phoemux()
 			end,
 			desc = 'Phoemux: open phoemux list'
+		},
+		---@type LazyKeysSpec
+		{
+			'<leader>pt',
+			function()
+				---@type Phoemux
+				local phoemux = require('phoemux-telescope')
+				phoemux.switch_tmux_session()
+			end,
+			desc = 'Phoemux: switch Tmux session'
+		},
+		{
+			'<leader>mk',
+			function()
+				---@type Phoemux
+				local phoemux = require('phoemux-telescope')
+				phoemux.kill_and_switch_session()
+			end,
+			desc = 'Phoemux: switch Tmux session and kill current one'
+
+		},
+		{
+			'<leader>pk',
+			function()
+				---@type Phoemux
+				local phoemux = require('phoemux-telescope')
+				phoemux.kill_session()
+			end,
+			desc = 'Phoemux: select a Tmux session to kill it'
+
+		},
+		{
+			'<leader>mt',
+			function()
+				---@type Phoemux
+				local phoemux = require('phoemux-telescope')
+				phoemux.kill_current_session()
+			end,
+			desc = 'Phoemux: kill current tmux session'
+
 		},
 	},
 }
