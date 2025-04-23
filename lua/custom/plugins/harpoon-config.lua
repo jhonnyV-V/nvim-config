8c99765bff72ebd12a61c124f779fc2e3cbec441
jhonnyV-V <jhonny_varela_visbal@hotmail.com>
2025-04-23 13:23:53 -0400

    formating

--- lua/custom/plugins/harpoon-config.lua
+++ lua/custom/plugins/harpoon-config.lua
@@ -1,11 +1,11 @@
 return {
 	'ThePrimeagen/harpoon',
 	keys = {
-		{'<leader>a', function () require('harpoon.mark').add_file() end,  desc = 'Add File to Harpoon'},
-		{'<C-e>', function () require('harpoon.ui').toggle_quick_menu() end,  desc = 'Open Harpoon Menu'},
-		{'<C-j>', function() require('harpoon.ui').nav_file(1) end,  desc = 'Harpoon file 1'},
-		{'<C-k>', function() require('harpoon.ui').nav_file(2) end,  desc = 'Harpoon file 2'},
-		{'<C-h>', function() require('harpoon.ui').nav_file(3) end,  desc = 'Harpoon file 3'},
-		{'<C-l>', function() require('harpoon.ui').nav_file(4) end,  desc = 'Harpoon file 4'},
+		{ '<leader>a', function() require('harpoon.mark').add_file() end,         desc = 'Add File to Harpoon' },
+		{ '<C-e>',     function() require('harpoon.ui').toggle_quick_menu() end,  desc = 'Open Harpoon Menu' },
+		{ '<C-j>',     function() require('harpoon.ui').nav_file(1) end,          desc = 'Harpoon file 1' },
+		{ '<C-k>',     function() require('harpoon.ui').nav_file(2) end,          desc = 'Harpoon file 2' },
+		{ '<C-h>',     function() require('harpoon.ui').nav_file(3) end,          desc = 'Harpoon file 3' },
+		{ '<C-l>',     function() require('harpoon.ui').nav_file(4) end,          desc = 'Harpoon file 4' },
 	},
 }
